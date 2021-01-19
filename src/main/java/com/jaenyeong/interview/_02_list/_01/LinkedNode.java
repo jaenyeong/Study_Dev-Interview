ee15d6fbcf98fe55ee21ee689e26af546711a988
jaenyeong <jaenyeong.dev@gmail.com>
2021-01-19 12:13:55 +0900

    Organize all packages

---
+++ src/main/java/com/jaenyeong/interview/_02_list/_01/LinkedNode.java
@@ -0,0 +1,10 @@
+package com.jaenyeong.interview._02_list._01;
+
+public class LinkedNode {
+    int number;
+    LinkedNode next;
+
+    public LinkedNode(int number) {
+        this.number = number;
+    }
+}
---
+++ src/main/java/com/jaenyeong/interview/_03_stack/_00_intro/StackIntro.java
@@ -0,0 +1,18 @@
+package com.jaenyeong.interview._03_stack._00_intro;
+
+import java.util.Stack;
+
+public class StackIntro {
+
+    public static void main(String[] args) {
+        Stack<Integer> numbers = new Stack<>();
+        numbers.push(1);
+        numbers.push(2);
+        numbers.push(3);
+
+        System.out.println(numbers.peek());
+        System.out.println(numbers.pop());
+        System.out.println(numbers.pop());
+        System.out.println(numbers.pop());
+    }
+}
