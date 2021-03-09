14e2746e336c17d96817325ba44297a03ce38814
jaenyeong <jaenyeong.dev@gmail.com>
2021-03-09 17:51:33 +0900

    Add find lowest common ancestor in tree problem

--- src/main/java/com/jaenyeong/interview/_05_tree/_03/FindLCA.java
+++ src/main/java/com/jaenyeong/interview/_05_tree/_03/FindLCA.java
@@ -1,5 +1,8 @@
 package com.jaenyeong.interview._05_tree._03;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindLCA {
 
     private static class Node {
@@ -19,7 +22,7 @@ public class FindLCA {
     }
 
     public static void main(String[] args) {
-        Node root = new Node(1);
+        final Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
@@ -27,17 +30,56 @@ public class FindLCA {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        FindLCA findLCA = new FindLCA();
-        System.out.println(findLCA.solution(root, 4, 5)); // 2
-        System.out.println(findLCA.solution(root, 2, 5)); // 2
-        System.out.println(findLCA.solution(root, 3, 4)); // 1
-        System.out.println(findLCA.solution(root, 3, 7)); // 3
+        final FindLCA findLCA = new FindLCA();
+        System.out.println(findLCA.getLCA(root, 4, 5)); // 2
+        System.out.println(findLCA.getLCA(root, 2, 5)); // 2
+        System.out.println(findLCA.getLCA(root, 3, 4)); // 1
+        System.out.println(findLCA.getLCA(root, 3, 7)); // 3
+        System.out.println(findLCA.getLCA(root, 3, 8)); // null
     }
 
     /**
      * TODO 주어진 이진 트리 (node) 에서 두 노드 n1, n2의 가장 가까운 공통 조상(Lowest Common Ancestor)을 찾는 코드를 작성하라.
      */
-    private Node solution(Node node, int n1, int n2) {
-        return null;
+    private Node getLCA(Node root, int n1, int n2) {
+        final List<Node> n1Path = new ArrayList<>();
+        final List<Node> n2Path = new ArrayList<>();
+
+        if (!findPath(root, n1, n1Path) || !findPath(root, n2, n2Path)) {
+            return null;
+        }
+
+        int idx = 0;
+        while (idx < n1Path.size() && idx < n2Path.size()) {
+            if (!n1Path.get(idx).equals(n2Path.get(idx))) {
+                break;
+            }
+            idx++;
+        }
+
+        return n1Path.get(idx - 1);
+    }
+
+    private boolean findPath(final Node node, final int targetValue, final List<Node> path) {
+        if (node == null) {
+            return false;
+        }
+
+        path.add(node);
+
+        if (node.value == targetValue) {
+            return true;
+        }
+
+        if (findPath(node.left, targetValue, path)) {
+            return true;
+        }
+
+        if (findPath(node.right, targetValue, path)) {
+            return true;
+        }
+
+        path.remove(node);
+        return false;
     }
 }
